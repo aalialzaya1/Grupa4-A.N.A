e3decfac6fd2876cd62f12d131796f670428970e
Amila Hrustić <ahrustic2@etf.unsa.ba>
2019-06-06 19:38:39 +0200

    proba

--- RentACar/RentACar/Models/ErrorViewModel.cs
+++ RentACar/RentACar/Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace RentACar.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+             
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
