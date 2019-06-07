8e0b47dba04cbe3a182e325c77fbb1e4e2164217
nimamovic <44224142+nimamovic@users.noreply.github.com>
2019-06-07 13:13:03 +0200

    ove neke klasice kreirane

---
+++ RentACar/RentACar/Models/Grad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace RentACar.Models
+{
+    public class Grad
+    {
+        private int id;
+        private String naziv;
+        public Grad()
+        {
+        }
+
+        public Grad(int id, string naziv)
+        {
+            this.id = id;
+            this.Naziv = naziv;
+        }
+
+        public int Id1 { get => id; set => id = value; }
+        public string Naziv1 { get => naziv; set => naziv = value; }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public String Naziv
+        {
+            get
+            {
+                return naziv;
+            }
+        }
+
+
+    }
+}
---
+++ RentACar/RentACar/Models/Rezervacija.cs
@@ -0,0 +1,118 @@
+﻿using System;
+namespace RentACar.Models
+{
+    public class Rezervacija
+    {
+        enum NacinPlacanja { Gotovina, katrica };
+        private int id;
+        private int brojDana;
+        private Klijent klijent;
+        private Vozilo voizlo;
+        private NacinPlacanja nacinPlacanja;
+        private Grad gradPreuzimanja;
+        private Grad gradVracanja;
+        private DateTime datumPreuzimanja;
+        private DateTime datumVracanja;
+
+        public int Id1 { get => id; set => id = value; }
+        public int BrojDana1 { get => brojDana; set => brojDana = value; }
+        public Klijent Klijent1 { get => klijent; set => klijent = value; }
+        public Vozilo Voizlo1 { get => voizlo; set => voizlo = value; }
+        private NacinPlacanja NacinPlacanja2 { get => nacinPlacanja; set => nacinPlacanja = value; }
+        public Grad GradPreuzimanja1 { get => gradPreuzimanja; set => gradPreuzimanja = value; }
+        public Grad GradVracanja1 { get => gradVracanja; set => gradVracanja = value; }
+        public DateTime DatumPreuzimanja1 { get => datumPreuzimanja; set => datumPreuzimanja = value; }
+        public DateTime DatumVracanja1 { get => datumVracanja; set => datumVracanja = value; }
+
+        public Rezervacija()
+        {
+        }
+
+        public Rezervacija(int id, int brojDana, Klijent klijent, Vozilo voizlo, NacinPlacanja nacinPlacanja, Grad gradPreuzimanja, Grad gradVracanja, DateTime datumPreuzimanja, DateTime datumVracanja)
+        {
+            this.Id = id;
+            this.BrojDana = brojDana;
+            this.Klijent = klijent;
+            this.Voizlo = voizlo;
+            this.NacinPlacanja1 = nacinPlacanja;
+            this.GradPreuzimanja = gradPreuzimanja;
+            this.GradVracanja = gradVracanja;
+            this.DatumPreuzimanja = datumPreuzimanja;
+            this.DatumVracanja = datumVracanja;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public int BrojDana
+        {
+            get
+            {
+                return brojDana;
+            }
+        }
+
+        public Klijent Klijent
+        {
+            get
+            {
+                return klijent;
+            }
+        }
+
+        public Vozilo Voizlo
+        {
+            get
+            {
+                return voizlo;
+            }
+        }
+
+
+        public NacinPlacanja NacinPlacanja
+        {
+            get
+            {
+                return nacinPlacanja;
+            }
+        }
+
+        public Grad GradPreuzimanja
+        {
+            get
+            {
+                return gradPreuzimanja;
+            }
+        }
+
+        public Grad GradVracanja
+        {
+            get
+            {
+                return gradVracanja;
+            }
+        }
+
+        public DateTime DatumPreuzimanja
+        {
+            get
+            {
+                return datumPreuzimanja;
+            }
+        }
+
+        public DateTime DatumVracanja
+        {
+            get
+            {
+                return datumVracanja;
+            }
+        }
+
+    }
+}
