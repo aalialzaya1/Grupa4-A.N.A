a9dc369538b5c07ed91c0df2606f7dd76cd4bf63
Amila Hrustić <ahrustic2@etf.unsa.ba>
2019-06-09 15:54:21 +0200

    dodana pocetna strana

---
+++ RentACar/RentACar/wwwroot/js/zaPocetnu.js
@@ -0,0 +1,35 @@
+﻿/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+  
+var instance = M.FormSelect.getInstance(elem);      
+
+
