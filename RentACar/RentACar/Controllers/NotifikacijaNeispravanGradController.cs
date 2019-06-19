118d1efadba1e0bca8f5f2d49ae201667e6998ad
nimamovic <44224142+nimamovic@users.noreply.github.com>
2019-06-19 19:40:27 +0200

    NASIHA RADI FRONTEND (sada i nikad visee) !!

---
+++ RentACar/RentACar/Controllers/NotifikacijaNeispravanGradController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class NotifikacijaNeispravanGradController : Controller
+    {
+        public IActionResult NotifikacijaNeispravanGrad()
+        {
+            return View();
+        }
+        public IActionResult NazadOnClick()
+        {
+            return View("../RezervacijaPrijavljen/RezervacijaPrijavljen");
+        }
+    }
+}
--- RentACar/RentACar/Controllers/RezervacijaPrijavljenController.cs
+++ RentACar/RentACar/Controllers/RezervacijaPrijavljenController.cs
@@ -22,42 +22,49 @@ namespace RentACar.Controllers
         {
             if (lokacijaVracanja != null && lokacijaPreuzimanja != null && datumDO != null && datumOD != null && imeIPrezime != null && datumRodjenja != null && brojTelefona != null)
             {
-                if (VozilaPrijavljenController.auto != null)
-                {
-                    var kliknutoVozilo = db.Vozila.Where((Vozilo v) => v.Naziv.Equals(VozilaPrijavljenController.auto));
-                    if (kliknutoVozilo.Count() != 0)
+                if ((lokacijaVracanja != "Sarajevo" && lokacijaVracanja != "Zenica" && lokacijaVracanja != "Tuzla") || (lokacijaPreuzimanja != "Sarajevo" && lokacijaPreuzimanja != "Zenica" && lokacijaPreuzimanja != "Tuzla")) {
+                    return View("../NotifikacijaNeispravanGrad/NotifikacijaNeispravanGrad");
+                }
+                else {
+                    if (VozilaPrijavljenController.auto != null)
                     {
-                        Vozilo voziloR = (Vozilo)kliknutoVozilo.First();
-                        voziloR.Dostupnost = false;
-                        db.Vozila.Update(voziloR);
-                        db.SaveChanges();
-                        //return View("../NotifikacijaUspjesnoRezervisano/NotifikacijaUspjesnoRezervisano");
-                        String[] datumPreuzimanja = datumOD.Split("/");
-                        String[] datumVracanja = datumDO.Split("/");
-                        DateTime datumPreuzimanjaOd = new DateTime(Int32.Parse(datumPreuzimanja[2]), Int32.Parse(datumPreuzimanja[1]), Int32.Parse(datumPreuzimanja[0]));
-                        DateTime datumVracanjaDo = new DateTime(Int32.Parse(datumVracanja[2]), Int32.Parse(datumVracanja[1]), Int32.Parse(datumVracanja[0]));
-                        int daniIzmedju = ((TimeSpan)(datumVracanjaDo - datumPreuzimanjaOd)).Days;
-                        var osobe = db.Osobe.Where((Osoba osoba) => osoba.Email.Equals(PrijavaController.prijavljen));
-                        Osoba osobaKojaRezervise = (Osoba)osobe.First();
-                        //DatumRodjenja = new DateTime(Int32.Parse(datum[2]), Int32.Parse(datum[1]), Int32.Parse(datum[0]))
-                        var gradoviPreuzimanja = db.Gradovi.Where((Grad grad) => grad.Naziv.Equals(lokacijaPreuzimanja));
-                        var gradoviVracanja = db.Gradovi.Where((Grad grad) => grad.Naziv.Equals(lokacijaVracanja));
-                        Grad gradPreuzimanja = gradoviPreuzimanja.First();
-                        Grad gradVracanja = gradoviVracanja.First();
-                        db.Rezervacije.Add(new Rezervacija                         {                             BrojDana = daniIzmedju,
-                            KlijentId = osobaKojaRezervise.Id,
-                            VoziloId = voziloR.id,
-                            NacinPlacanja1 = (int)Rezervacija.NacinPlacanja.SADA,
-                            GradPreuzimanjaId = gradPreuzimanja.Id,
-                            GradVracanjaId = gradVracanja.Id,
-                            DatumPreuzimanja = datumPreuzimanjaOd,
-                            DatumVracanja = datumVracanjaDo
+                        var kliknutoVozilo = db.Vozila.Where((Vozilo v) => v.Naziv.Equals(VozilaPrijavljenController.auto));
+                        if (kliknutoVozilo.Count() != 0)
+                        {
+                            Vozilo voziloR = (Vozilo)kliknutoVozilo.First();
+                            voziloR.Dostupnost = false;
+                            db.Vozila.Update(voziloR);
+                            db.SaveChanges();
+                            //return View("../NotifikacijaUspjesnoRezervisano/NotifikacijaUspjesnoRezervisano");
+                            String[] datumPreuzimanja = datumOD.Split("/");
+                            String[] datumVracanja = datumDO.Split("/");
+                            DateTime datumPreuzimanjaOd = new DateTime(Int32.Parse(datumPreuzimanja[2]), Int32.Parse(datumPreuzimanja[1]), Int32.Parse(datumPreuzimanja[0]));
+                            DateTime datumVracanjaDo = new DateTime(Int32.Parse(datumVracanja[2]), Int32.Parse(datumVracanja[1]), Int32.Parse(datumVracanja[0]));
+                            int daniIzmedju = ((TimeSpan)(datumVracanjaDo - datumPreuzimanjaOd)).Days;
+                            var osobe = db.Osobe.Where((Osoba osoba) => osoba.Email.Equals(PrijavaController.prijavljen));
+                            Osoba osobaKojaRezervise = (Osoba)osobe.First();
+                            //DatumRodjenja = new DateTime(Int32.Parse(datum[2]), Int32.Parse(datum[1]), Int32.Parse(datum[0]))
+                            var gradoviPreuzimanja = db.Gradovi.Where((Grad grad) => grad.Naziv.Equals(lokacijaPreuzimanja));
+                            var gradoviVracanja = db.Gradovi.Where((Grad grad) => grad.Naziv.Equals(lokacijaVracanja));
+                            Grad gradPreuzimanja = gradoviPreuzimanja.First();
+                            Grad gradVracanja = gradoviVracanja.First();
+                            db.Rezervacije.Add(new Rezervacija
+                            {
+                                BrojDana = daniIzmedju,
+                                KlijentId = osobaKojaRezervise.Id,
+                                VoziloId = voziloR.id,
+                                NacinPlacanja1 = (int)Rezervacija.NacinPlacanja.SADA,
+                                GradPreuzimanjaId = gradPreuzimanja.Id,
+                                GradVracanjaId = gradVracanja.Id,
+                                DatumPreuzimanja = datumPreuzimanjaOd,
+                                DatumVracanja = datumVracanjaDo
 
-                        });
-                        db.SaveChanges();
-                        return View("..//NotifikacijaUspjesnoRezervisano/NotifikacijaUspjesnoRezervisano");
+                            });
+                            db.SaveChanges();
+                            return View("..//NotifikacijaUspjesnoRezervisano/NotifikacijaUspjesnoRezervisano");
                         }
 
+                    }
                 }
             }
             return View("../NotifikacijaRezervacije2/NotifikacijaRezervacije2");
